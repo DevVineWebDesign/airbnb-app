4fcdd49db5361b0fc9a3eb81490263e4b9820ad6
DevVineWebDesign <devvinewebdesign@gmail.com>
2023-04-11 23:39:04 -0400

    4/11/2023 11:38

--- airbnb-app/app/components/Button.tsx
+++ airbnb-app/app/components/Button.tsx
@@ -10,11 +10,34 @@ interface ButtonProps {
   icon?: IconType;
 }
 
-
-const Button = () => {
+const Button: React.FC<ButtonProps> = ({
+  label,
+  onClick,
+  disabled,
+  outline,
+  small,
+  icon
+}) => {
   return (
-    <button></button>
-  );
+    <button className={`
+      relative
+      disabled:opacity-70
+      disabled:cursor-not-allowed
+      rounded-lg
+      hover:opacity-80
+      transition
+      w-full
+      ${outline ? 'bg-white' : 'bg-rose-500'}
+      ${outline ? 'border-black' : 'border-rose-500'}
+      ${outline ? 'text-black' : 'text-white'}
+      ${small ? 'py-1' : 'py-3'}
+      ${small ? 'text-sm' : 'text-md'}
+      ${small ? 'font-light' : 'font-semibold'}
+    `}>
+      {label}
+    </button>
+    );
 }
+
 
 export default Button;
