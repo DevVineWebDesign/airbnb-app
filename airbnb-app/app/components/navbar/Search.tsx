b4e66de65c08d9bf978262d3f958fa8359f1ac80
DevVineWebDesign <devvinewebdesign@gmail.com>
2023-04-11 23:06:56 -0400

    4/11/23

--- airbnb-app/app/components/navbar/Search.tsx
+++ airbnb-app/app/components/navbar/Search.tsx
@@ -15,10 +15,9 @@ const Search = () => {
         </div>
 
         <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-          <div className="gidden sm:block">Add Guests
+          <div className="hidden sm:block">Add Guests</div>
           <div className="p-2 bg-rose-500 rounded-full text-white">
             <BiSearch size={18} />
-          </div>
          </div>
         </div>
       </div>
