d12e832c1a87ecef801683cf786ecc62eedb8b5a
DevVineWebDesign <devvinewebdesign@gmail.com>
2023-04-16 10:55:44 -0400

    4/16/2023-10:55

--- airbnb-app/app/components/modals/RegisterModal.tsx
+++ airbnb-app/app/components/modals/RegisterModal.tsx
@@ -10,7 +10,8 @@ import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
-
+import { toast } from 'react-hot-toast';
+import Button from '../Button';
 
 
 const RegisterModal = () => {
@@ -39,7 +40,7 @@ const onSubmit: SubmitHandler<FieldValues> = (data) => {
       registerModal.onClose();
     })
     .catch((error) => {
-      console.log(error);
+      toast.error('Something Went Wrong');
     })
     .finally(() => {
       setIsLoading(false);
@@ -79,6 +80,41 @@ const bodyContent = (
       required
     />
   </div>
+);
+
+const footerContent =(
+  <div className="flex flex-col gap-4 mt-3">
+    <hr />
+    <Button
+      outline
+      label="Continue with Google"
+      icon={FcGoogle}
+      onClick={() => {}}
+    />
+    <Button
+      outline
+      label="Continue with Github"
+      icon={AiFillGithub}
+      onClick={() => {}}
+    />
+    <div className="
+      text-neutral-500
+      text-center
+      mt-4
+      font-light
+    ">
+      <div className="justify-center flex flex-row items-center gap-2">
+        <div>
+          Already have an account?
+        </div>
+        <div 
+        onClick={registerModal.onClose}
+         className="text-neutral-800 cursor-pointer hover:underline">
+          Log in
+        </div>
+      </div>
+    </div>
+  </div>
 )
 
   return (
@@ -90,6 +126,7 @@ const bodyContent = (
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 }
